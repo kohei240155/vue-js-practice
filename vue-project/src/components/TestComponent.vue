3851deeffef188e488650c1094c6e95f601b01e4
kohei240155 <k.pino519@gmail.com>
2025-04-06 16:10:25 +0900

    属性バインディングのサンプルを追加

--- vue-project/src/components/TestComponent.vue
+++ vue-project/src/components/TestComponent.vue
@@ -3,9 +3,16 @@ defineProps({
     text: String,
 });
 const dataTest = "これはデータです！"
+const imageUrl = "/sample.png"
 </script>
 
 <template>
+    <!-- テキストバインディング(マスタッシュ構文) -->
     <p>{{ dataTest }}</p>
+    <!-- テキストバインディング(通常) -->
     <p v-text="text"></p>
+    <!-- 属性バインディング -->
+    <img v-bind:src="imageUrl" />
+    <!-- 属性バインディング(省略版) -->
+    <img :src="imageUrl"/>
 </template>
