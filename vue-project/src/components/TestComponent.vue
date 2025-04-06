86a4433ce5596d19c0e71b6bb966165c8712c8d2
kohei240155 <k.pino519@gmail.com>
2025-04-06 16:14:29 +0900

    イベントバインディングのサンプルを追加

--- vue-project/src/components/TestComponent.vue
+++ vue-project/src/components/TestComponent.vue
@@ -4,6 +4,9 @@ defineProps({
 });
 const dataTest = "これはデータです！"
 const imageUrl = "/sample.png"
+function myMethod() {
+    console.log("メソッド実行")
+}
 </script>
 
 <template>
@@ -15,4 +18,8 @@ const imageUrl = "/sample.png"
     <img v-bind:src="imageUrl" />
     <!-- 属性バインディング(省略版) -->
     <img :src="imageUrl"/>
+    <!-- イベントバインディング -->
+    <button v-on:click="myMethod">メソッド実行</button>
+    <!-- イベントバインディング(省略版) -->
+    <button @click="myMethod">メソッド実行</button>
 </template>
